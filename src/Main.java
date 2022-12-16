33acebf749e74765f5daf4f828b90f4a9bf22722
MarDom92 <marcin.domanski92@gmail.com>
2022-12-16 22:23:47 +0100

    get size from user

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        int size = getNumberFromUser();
+    }
+
+    private static int getNumberFromUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj końcowy przedział zakresu, dla którego wyznaczyć liczby pierwsze:");
+        return scanner.nextInt();
     }
 }
